36774a078e076fc744a9a9dd1f92d1d412dcf3fa
ATmilkyway <natnaelasegd0507@gmail.com>
2025-12-30 21:45:20 +0300

    feat: update query setting

--- src/react-query/hooks/useTodos.ts
+++ src/react-query/hooks/useTodos.ts
@@ -8,14 +8,18 @@ interface Todo {
   userId: number;
   completed: boolean;
 }
-    
+
 const useTodos = () =>
   useQuery<Todo[], Error>({
     queryKey: ["todos"],
     queryFn: () =>
       axios
-        .get<Todo[]>("https://jsonplaceholder.typicode.com/todos0")
+        .get<Todo[]>("https://jsonplaceholder.typicode.com/todos")
         .then((res) => res.data),
+    refetchOnWindowFocus: false,
+    staleTime: 1_000 * 1,
+    retry: 3,
+    gcTime: 300_000,
   });
 
 export default useTodos;
