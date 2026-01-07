d2f66fe3a432c4aedddd90fd7657af25799d85c1
ATmilkyway <natnaelasegd0507@gmail.com>
2026-01-08 00:30:29 +0300

    Link and Navigation

--- src/routing/ContactPage.tsx
+++ src/routing/ContactPage.tsx
@@ -1,9 +1,13 @@
+import { useNavigate } from "react-router-dom";
+
 const ContactPage = () => {
+  const naviagte = useNavigate();
   return (
     <form
       onSubmit={(event) => {
         event.preventDefault();
         // Redirect the user to the home page
+        naviagte("/");
       }}
     >
       <button className="btn btn-primary">Submit</button>
