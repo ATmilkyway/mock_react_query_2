c2fd606efcf8c95874810431cc24a0e8253a27fd
ATmilkyway <natnaelasegd0507@gmail.com>
2026-01-08 11:30:22 +0300

    error handling

--- src/routing/HomePage.tsx
+++ src/routing/HomePage.tsx
@@ -1,13 +1,14 @@
 import { Link } from "react-router-dom";
 
 const HomePage = () => {
+  // throw new Error("Errrrrrrrrrrrrrrrrrrrrrrrr" );
   return (
     <>
       <p>
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-        Incidunt, mollitia!
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Incidunt,
+        mollitia!
       </p>
-      <Link to="/users" >Users</Link>
+      <Link to="/users">Users</Link>
       {/* <a href="/users">Users</a> */}
     </>
   );
